fb1218518c4398213f7ec536b25323fb9e215490
tianling888 <107652555+tianling888@users.noreply.github.com>
2023-01-04 14:13:58 +0800

    Update sign_in.go

--- plugin/score/sign_in.go
+++ plugin/score/sign_in.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	backgroundURL = "https://img.moehu.org/pic.php?id=pc"
+	backgroundURL = "http://img.xjh.me/random_img.php"
 	signinMax     = 1
 	// SCOREMAX 分数上限定为1200
 	SCOREMAX = 1200
